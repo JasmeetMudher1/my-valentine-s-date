0bb464a9b38ce1110146a534bf1efa841f91e484
JasmeetMudher1 <jsmudher57@gmail.com>
2026-01-31 11:36:19 +0300

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/my-valentine-s-date/',
   server: {
     host: "::",
     port: 8080,
